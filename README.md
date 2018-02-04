e9b526fa5ab1071e20afdfe3077c44731ad52b1c
Meryem Mansouri Idrissi <midri.meryem@gmail.com>
2018-02-04 17:30:54 +0000

    DAY 2
    Mongo is a document database that stores JSON like objects,
    	The Model (also called schema defines which elements are in that object)
    		File created: User.js
    
    		Our schema is written using the available one on source N1
    
    
    		- Testing gave errors, and din't run register command
    		- Start with another tutorial: N2

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # H-NDIR
 ## Day 1: Authentication
 	Packages needed:
-		-Express : to run the app
-		-Nodemon : restarting the server whenever a change occurs 
-		-Mongoose : to simplify the interaction with MongoDB
-		-Bcrypt: for hashing and salting passwords
-		-Express session : to handle sessions
-		-Connect-Mongo : for storing sessions in MongoDB
+		- Express : to run the app
+		- Nodemon : restarting the server whenever a change occurs 
+		- Mongoose : to simplify the interaction with MongoDB
+		- Bcrypt: for hashing and salting passwords
+		- Express session : to handle sessions
+		- Connect-Mongo : for storing sessions in MongoDB
+		File: Index.js
+
+## Day 2: Database Linking
+	Mongo is a document database that stores JSON like objects,
+	The Model (also called schema defines which elements are in that object)
+		File created: User.js
+
+		Our schema is written using the available one on source N1
+
+
+		- Testing gave errors, and din't run register command
+		- Start with another tutorial: N2
--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 /*Required Modules*/
 var express = require('express');
 var bodyParser = require('body-parser'); 
+var router = express.Router();
+var User = require('C:/Users/musul/Desktop/H-NDIR/User');
+
 //Not installed using npm install as it is a core module
 //It is installed with Node, can be found in lib/
 var path = require('path');
 
 //initialization: doesn't have to be named as the file
 var app = express();
-
 //Args: Port to listen to, Call back fcn
  app.listen(3000, function(){
 	console.log('Server Started on Port 3000....');
-})
+});
+
+//Get home page
+router.get('/', function(req, res, next){
+	res.render('index', {title: 'Express'});
+});
+
+
+router.post('/register', function(req, res){
+	if(req.body.email && req.body.username && req.body.password && req.body.passwordConf){
+		var userdata = {
+			email: req.body.email,
+			username: req.body.username,
+			password: req.body.password,
+			passwordConf: req.body.passwordConf,
+		}	
+	}
+	//Insert data on DB using schema.create
+	User.create(userdata, function(err, User){
+		if(err){
+			return next(err)
+		}
+		else {
+			//Successful Login
+			return res.redirect('/profile');
+		}
+	});
+
+});
+
+module.exports = router;
+
+	
+
+	
